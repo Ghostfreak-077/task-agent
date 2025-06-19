2fc473f47c797ded0b610cd71d202007cc50bfeb
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2025-06-19 10:28:14 +0530

    add: folder structure and packages

---
+++ tools/tavily.py
@@ -0,0 +1,6 @@
+from langchain_tavily import TavilySearch
+
+web_search = TavilySearch(max_results=3)
+web_search_results = web_search.invoke("who is the mayor of NYC?")
+
+print(web_search_results["results"][0]["content"])
